1367538f532027f63279a353d20511a34a9d260d
diseyi <filomiinaa@gmail.com>
2022-07-24 16:05:00 +0100

    change header color

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
   return (
     <div className=" bg-[#e2eafc] h-screen ">
       <div className="text-center text-3xl font-bold py-4 capitalize">click on the chat icon</div>
-      <div className="fixed bottom-10 right-0 md:right-4">
+      <div className="md:fixed md:bottom-10 right-0 md:right-4">
         {!showChatbox && (
           <div
             className="w-16 h-16 rounded-full bg-[#05445E] mx-2 my-2 flex items-center justify-center"
@@ -25,7 +25,7 @@ function App() {
         )}
 
         {showChatbox && (
-          <div className="relative m-auto md:w-96 h-[600px]  rounded shadow-lg overflow-none bg-[#fef9ef] border-[#189AB4] mt-10 mx-auto">
+          <div className="relative m-auto md:w-96 h-[600px]  rounded shadow-lg overflow-none bg-[#ffffff] border-[#189AB4] mt-10 mx-auto">
             <Header handleClick={ () => setShowChatbox(false) } />
             <div className="w-full h-[510px] overflow-y-auto">
               <Chatbox />
